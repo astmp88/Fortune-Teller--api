9f8dc0f334318fa5482a3330b20415fc163028f3
Astrid Miranda <mounphant@gmail.com>
2025-12-05 11:12:06 +0100

    Prepare for Render deployment

--- apif.py
+++ apif.py
@@ -61,4 +61,4 @@ def fortune():
     return jsonify({"fortune": fortune_cookie})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
